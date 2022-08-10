bb141733ada6c4c02ec1ba4efe1cb2ddeccbaf3f
André <andregds85@gmail.com>
2022-08-10 18:35:33 -0300

    Modificando Tudo

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,47 +3,37 @@
 @section('content')
 
 
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Alterar Senha</h2>
-        </div>
-      
-    </div>
-</div>
-
-@if (count($errors) > 0)
-  <div class="alert alert-danger">
-    <strong>Ops!</strong> Houve alguns Problemas com a sua entrada<br><br>
-    <ul>
-       @foreach ($errors->all() as $error)
-         <li>{{ $error }}</li>
-       @endforeach
-    </ul>
-  </div>
-@endif
 
 
 {!! Form::model($user, ['method' => 'PATCH','route' => ['users.update', $user->id]]) !!}
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Nome:</strong>
+            {!! Form::text('name', null, array('placeholder' => 'Nome','class' => 'form-control')) !!}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Email:</strong>
+            {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+        </div>
+    </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             <strong>Senha:</strong>
-            {!! Form::password('password', array('placeholder' => 'Senha','class' => 'form-control')) !!}
+            {!! Form::password('confirm-password', array('placeholder' => 'Confirmar Senha','class' => 'form-control')) !!}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
-            <strong>Confirmar Senha:</strong>
-            {!! Form::password('confirm-password', array('placeholder' => 'Confirmar Senha','class' => 'form-control')) !!}
+            <strong>Grupo:</strong>
+            {!! Form::select('roles[]', $roles,$userRole, array('class' => 'form-control','multiple')) !!}
         </div>
     </div>
 
-
-
-
-
     <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        <button type="submit" class="btn btn-primary">Alterar  Senha</button>
+        <button type="submit" class="btn btn-primary">Alterar</button>
     </div>
 </div>
 {!! Form::close() !!}
